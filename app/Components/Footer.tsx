46f134ffa1cadbc24591d93831aa94dac9000729
Noel <beltran.d.noel@gmail.com>
2026-06-08 16:13:06 -0700

    Footer

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,10 +1,29 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <footer className="w-full bg-primary py-6 mt-auto">
-            
-            <div className="flex flex-col items-center justify-center gap-2 max-w-4xl mx-auto text-center text-sm text-[#FAF8F4]">
-                <p>&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
-                <p>Desarrollado por e-commetrics.com</p>    
+        <footer className=" bg-primary">
+            <div className="py-6 mt-auto mx-10">
+                <div className="flex flex-row items-center justify-between">
+                    <h1 className="text-lg font-semibold tracking-[0.2em] uppercase text-[#FAF8F4] mb-4">
+                        Enrique Ciapara
+                    </h1>
+                    <div className="text-sm text-[#FAF8F4]">
+                        <Link href="/" className="hover:text-amber-500 transition-colors font-bold text-lg">
+                            Inicio
+                        </Link>
+                    </div>
+                </div>
+                <hr className="border-[#FAF8F4]" />
+                <div className="flex flex-col items-center justify-center gap-2 max-w-4xl mx-auto text-center text-sm text-[#FAF8F4] mt-4">
+                    <p>&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
+                    <div>
+                        <span className="font-medium">Desarrollado por </span>
+                        <a href="https://e-commetrics.com/" className="text-amber-500 hover:text-amber-600 transition-colors">
+                            e-commetrics
+                        </a>
+                    </div>
+                </div>
             </div>
         </footer>
     )
