1a52dd7ad03bf0606872f6f1975209d1c4521baa
NoelBeltranD <noel@e-commetrics.com>
2026-06-09 14:41:36 -0700

    About

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -83,7 +83,7 @@ export default function About() {
           />
         </figure>
       </div>
-      <div className="max-w-7xl mx-auto">
+      {/* <div className="max-w-7xl mx-auto">
         <div className="grid md:grid-cols-2 gap-16 lg:gap-24 items-start">
           <div>
             <motion.p
@@ -144,7 +144,7 @@ export default function About() {
             </motion.div>
           </div>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
